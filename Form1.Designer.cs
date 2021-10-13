1d72b6e0a873ee395865a438063926f16437b60c
P4 <umeaansa555@ed.amdsb.ca>
2021-10-13 14:36:54 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace AverageValuesPractice
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(154, 96);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(130, 20);
+            this.inputBox.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(298, 96);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(120, 19);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(128, 171);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(82, 13);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "Enter a number.";
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.Location = new System.Drawing.Point(298, 257);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(120, 19);
+            this.resetbutton.TabIndex = 3;
+            this.resetbutton.Text = "Reset";
+            this.resetbutton.UseVisualStyleBackColor = true;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 398);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.inputBox);
+            this.Name = "form1";
+            this.Text = "Reset";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button resetbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageValuesPractice
+{
+    public partial class form1 : Form
+    {
+        int sum;
+        int totalNumbers;
+        int avg;
+
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            sum = Convert.ToInt32(inputBox.Text);
+            avg = (sum / totalNumbers);
+
+            if(sum != 0)
+            {
+                //fill this in
+            }
+            else
+            {
+                outputLabel.Text = $"The average of all numbers entered is: {avg}";
+            }
+        }
+
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            totalNumbers = 0;
+        }
+    }
+}
